2db92befc2c4f9c8a0105a3e2886e702c3ed86ae
badaohenzx <158378340@qq.com>
2016-07-16 18:04:25 +0800

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("测试");
-        System.out.println("测试2");
+        System.out.println("测试2fa");
     }
 }
